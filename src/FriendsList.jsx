8d22c558e5642d773c0bd1099003882e4d2ac24c
Paul Cristea <cristeapaul.2015@gmail.com>
2025-01-03 18:30:13 +0200

    feat(FriendComp): Set selected class acording to friend selection

--- src/FriendsList.jsx
+++ src/FriendsList.jsx
@@ -1,11 +1,16 @@
 import Friend from "./Friend";
 
-function FriendsList({ friends }) {
+function FriendsList({ friends, onSelection, selectedFriend }) {
   return (
     <div className="sidebar">
       <ul>
         {friends.map((friend) => (
-          <Friend key={friend.id} friend={friend} />
+          <Friend
+            key={friend.id}
+            friend={friend}
+            onSelection={onSelection}
+            selectedFriend={selectedFriend}
+          />
         ))}
       </ul>
     </div>
