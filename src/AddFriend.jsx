60cce9d25e126fdfb6427fea5d26c9ab80915165
Paul Cristea <cristeapaul.2015@gmail.com>
2025-01-03 17:41:10 +0200

    feat(Project): Added AddFriend addinc controlled elements

--- src/AddFriend.jsx
+++ src/AddFriend.jsx
@@ -4,13 +4,32 @@ import Button from "./Button";
 function AddFriend() {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
+
+  function handleSubmit(e) {
+    e.prventDefault();
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?u=${id}`,
+      balance: 0,
+    };
+  }
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑 Friend</label>
-      <input type="text" value={name} onChange={setName} />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🖼️ Image</label>
-      <input type="text" value={image} onChange={setImage} />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
